a0250b723f517bfbb2ef289b78dcc9bd3b9074a1
Kamrul Huda Sattari Saad <skamrul2680@gmail.com>
2022-04-15 14:47:39 +0600

    Firebase key Hidden

--- firebase.init.js
+++ firebase.init.js
@@ -5,12 +5,12 @@ import { initializeApp, getAuth } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyCxewCgIZZiM5MuKWpV6O3RBsoDcHZyMBI",
-  authDomain: "genius-car-services-10ca1.firebaseapp.com",
-  projectId: "genius-car-services-10ca1",
-  storageBucket: "genius-car-services-10ca1.appspot.com",
-  messagingSenderId: "489188634583",
-  appId: "1:489188634583:web:75e455fa26d8fe794fa592"
+  apiKey: process.env.REACT_APP_apiKey ,
+  authDomain: process.env.REACT_APP_authDomain ,
+  projectId: process.env.REACT_APP_projectId ,
+  storageBucket: process.env.REACT_APP_storageBucket ,
+  messagingSenderId: process.env.REACT_APP_messagingSenderId ,
+  appId: process.env.REACT_APP_appId 
 };
 
 // Initialize Firebase
