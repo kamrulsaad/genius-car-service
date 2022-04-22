62818c031a92b54b8fbf8993b01971e2557c7b6b
Kamrul Huda Sattari Saad <skamrul2680@gmail.com>
2022-04-22 20:37:24 +0600

    Added Post system

--- src/Pages/AddServices/AddServices.js
+++ src/Pages/AddServices/AddServices.js
@@ -4,15 +4,30 @@ import PageTitle from '../Shared/PageTItle/PageTitle';
 
 const AddServices = () => {
     const { register, handleSubmit } = useForm();
-    const onSubmit = data => console.log(data);
+    const onSubmit = data => {
+        const url = "http://localhost:5000/services"
+        fetch(url, {
+            method: 'POST',
+            headers: {
+                'content-type' : 'application/json'
+            },
+            body : JSON.stringify(data)
+        })
+        .then(res => res.json())
+        .then(data => console.log(data))
+    }
 
     return (
         <form onSubmit={handleSubmit(onSubmit)}>
+            <h1 className="text-center">Please Add a Service</h1>
             <PageTitle page='Add New Service'></PageTitle>
-            <input {...register("firstName", { required: true, maxLength: 20 })} />
-            <input {...register("lastName", { pattern: /^[A-Za-z]+$/i })} />
-            <input type="number" {...register("age", { min: 18, max: 99 })} />
-            <input type="submit" />
+            <div className="d-flex flex-column mx-auto gap-3 w-50">
+                <input placeholder='Name' className='p-2 border-1 rounded text-uppercase' {...register("name")} />
+                <textarea placeholder='Description' className='p-2 border-1 rounded' {...register("description")} />
+                <input placeholder='Price' className='p-2 border-1 rounded' type="number" {...register("price")} />
+                <input placeholder='Photo URL' className='p-2 border-1 rounded' {...register("img")} />
+                <input placeholder='' className='p-2 border-1 rounded bg-primary text-white' type="submit" value='Add New Service'/>
+            </div>
         </form>
     );
 };
